e3a6e4eef3e303e6da5917095a482aac7b869325
PHUICMT <PHUICMT@github.com>
2021-08-23 10:26:51 +0700

    📝 Save result to database

--- app/app.py
+++ app/app.py
@@ -12,10 +12,10 @@ def upload_webcam_file():
     uuid = request.form.get("uuid", False)
     filename = "["+uuid+"]"+'webcam.webm'
     save_file(uploaded_file, uuid, True, filename)
-    # result = run_predict('./app/video_storage/'+filename)
-    # result_json = json.dumps(result)
-    # save_result_to_database(uuid,result_json)
-    return jsonify({"response": "webcam"}), 200
+    result = run_predict('./app/video_storage/'+filename)
+    result_json = json.dumps(result)
+    save_result_to_database(uuid,result_json)
+    return jsonify({"result": result}), 200
 
 @app.route('/upload-recorded-screen', methods=['POST'])
 def upload_screen_file():
