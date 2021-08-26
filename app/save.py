013034d4aa5f0cc387543e59676dca693462e2b0
PHUICMT <PHUICMT@github.com>
2021-08-26 15:58:50 +0700

    🐛 Fix save to db

--- app/save.py
+++ app/save.py
@@ -20,7 +20,7 @@ current_time = now.strftime('%Y-%m-%d %H:%M:%S')
 def save_file(file, questionnaire_id, video_type, filename):
     file.save(os.path.join(UPLOAD_FOLDER, filename))
 
-    sql_insert_blob_query = " INSERT INTO videos (id, video_name, questionnaire_id, video_type_is_webcam) VALUES (%s,%s,%s,%s)"
+    sql_insert_blob_query = " INSERT INTO Videos (id, video_name, questionnaire_id, video_type_is_webcam) VALUES (%s,%s,%s,%s)"
     insert_blob_tuple = (video_uuid, filename, questionnaire_id, video_type)
     result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
     mydb.commit()
@@ -28,7 +28,7 @@ def save_file(file, questionnaire_id, video_type, filename):
 
 
 def save_questionnaire_to_database(questionnaire_id):
-    sql_insert_query = " INSERT INTO questionnaire (id, created_at) VALUES (%s,%s)"
+    sql_insert_query = " INSERT INTO Questionnaire (id, created_at) VALUES (%s,%s)"
     insert_tuple = (questionnaire_id, current_time)
     result = cursor.execute(sql_insert_query, insert_tuple)
     mydb.commit()
@@ -36,7 +36,7 @@ def save_questionnaire_to_database(questionnaire_id):
 
 
 def save_result_to_database(questionnaire_id, emotion, answer, events):
-    sql_insert_query = " INSERT INTO result (questionnaire_id, emotion) VALUES (%s,%s)"
+    sql_insert_query = " INSERT INTO Result (Questionnaire_id, emotion) VALUES (%s,%s)"
     insert_tuple = (questionnaire_id, emotion, answer, events)
     result = cursor.execute(sql_insert_query, insert_tuple)
     mydb.commit()
@@ -44,7 +44,7 @@ def save_result_to_database(questionnaire_id, emotion, answer, events):
 
 
 def save_result_fontend(questionnaire_id, answer, events):
-    sql_insert_query = " INSERT INTO result (questionnaire_id,  answer, events) VALUES (%s,%s,%s)"
+    sql_insert_query = " INSERT INTO Result (questionnaire_id,  answer, events) VALUES (%s,%s,%s)"
     insert_tuple = (questionnaire_id,  answer, events)
     result = cursor.execute(sql_insert_query, insert_tuple)
     mydb.commit()
