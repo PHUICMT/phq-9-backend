da3921d19cdd3e21eca3148cb9c9f97b2fb18fcb
PHUICMT <PHUICMT@github.com>
2021-10-24 22:03:09 +0700

    📝 Change DB Video format

--- app/save.py
+++ app/save.py
@@ -1,11 +1,9 @@
 import os
-import uuid
 from datetime import datetime
 import mysql.connector
 
 UPLOAD_FOLDER = './app/video_storage'
 UPLOAD_IMAGES = './app/images/results'
-video_uuid = str(uuid.uuid4())
 mydb = mysql.connector.connect(
     host="db",
     user="admin",
@@ -20,8 +18,8 @@ current_time = now.strftime('%Y-%m-%d %H:%M:%S')
 
 def save_video(file, questionnaire_id, video_type, filename):
     file.save(os.path.join(UPLOAD_FOLDER, filename))
-    sql_insert_blob_query = " INSERT INTO Videos (id, video_name, questionnaire_id, video_type_is_webcam) VALUES (%s,%s,%s,%s)"
-    insert_blob_tuple = (video_uuid, filename, questionnaire_id, video_type)
+    sql_insert_blob_query = " INSERT INTO Videos (video_name, questionnaire_id, video_type_is_webcam) VALUES (%s,%s,%s)"
+    insert_blob_tuple = (filename, questionnaire_id, video_type)
     result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
     mydb.commit()
     return 'Saved! and Save to Database'
